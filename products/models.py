c65f9aea86743efbbd0133a8c7a549ba7124ceb9
Huynh Phuc Nghi <nghiktkt22@gmail.com>
2019-12-12 06:47:12 +0700

    Fix type bugs

--- products/models.py
+++ products/models.py
@@ -15,8 +15,8 @@ from django.utils import timezone
 class Event(models.Model):
     name = models.CharField(max_length=200)
     content = models.TextField(max_length=1000)
-    start_date = models.DateTimeField('start date')
-    end_date = models.DateTimeField('end date')
+    start_date = models.DateTimeField('start date', default=timezone.now)
+    end_date = models.DateTimeField('end date', default=timezone.now)
 
     def __str__(self):
         return self.name
@@ -28,7 +28,8 @@ class DicountCode(models.Model):
     discount_type = models.IntegerField(default=0)
     discount_rate = models.FloatField()
     discount_max = models.PositiveIntegerField()
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    event = models.ForeignKey(
+        Event, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -38,26 +39,29 @@ class ProductCategory(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=1000)
     status = models.PositiveSmallIntegerField()
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    event = models.ForeignKey(
+        Event, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
-        return self.id
+        return self.name
 
 
 class ProductLine(models.Model):
     attribute = models.CharField(max_length=200)
     price = models.PositiveIntegerField()
-    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    category = models.ForeignKey(
+        ProductCategory, on_delete=models.CASCADE, blank=True, null=True)
 
     def __str__(self):
-        return self.id
+        return str(self.id)
 
 
 class Product(models.Model):
     seri_number = models.CharField(max_length=200)
     manufacturing_date = models.DateTimeField('manufacturing date')
     config = models.TextField(max_length=1000)
-    line = models.ForeignKey(ProductLine, on_delete=models.CASCADE)
+    line = models.ForeignKey(
+        ProductLine, on_delete=models.CASCADE, blank=True, null=True)
     # zone = models.ForeignKey(Zone, on_delete=models.CASCADE)
     # waybill = models.ForeignKey(Waybill, on_delete_models.CASCADE)
 
